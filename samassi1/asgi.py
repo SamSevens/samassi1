8ee7c55ebb56db83e072f1cdde53f9dcba17dfa5
samsevens <samrosemount@gmail.com>
2022-10-04 14:53:02 -0400

    file name fixed

--- samassi1/asgi.py
+++ samassi1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for assignment1sam project.
+ASGI config for samassi1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment1sam.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samassi1.settings')
 
 application = get_asgi_application()
--- samassi1/wsgi.py
+++ samassi1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for assignment1sam project.
+WSGI config for samassi1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment1sam.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samassi1.settings')
 
 application = get_wsgi_application()
